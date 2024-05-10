423ce31323235477314775898efccbaf0dcb5591
Josh Kang <3375278+kngroo@users.noreply.github.com>
2024-05-10 12:14:57 -0400

    Add email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,28 @@ export default function Home() {
       >
         <source src="/moto.mp4" type="video/mp4" />
       </video>
-      <main className="whitespace-nowrap relative uppercase flex flex-col items-center justify-center w-full [text-shadow:_1px_1px_0_rgb(0_0_0_/_100%)] select-none top-1/4 sm:h-full sm:top-0">
+      <main className="whitespace-nowrap relative uppercase flex flex-col items-center justify-center [text-shadow:_1px_1px_0_rgb(0_0_0_/_100%)] select-none top-1/4 sm:h-full sm:top-0">
         <h1 className="text-2xl sm:text-3xl">Three Horses</h1>
         <p className="text-4xl sm:text-5xl">Pouring Soon</p>
         <h2 className="text-lg sm:text-xl mt-1">Cafe & Speed Shop</h2>
-        <h3 className="text-lg sm:text-xl">
-          <a href="https://maps.app.goo.gl/rM8onc3vVnmvAodc9" target="_blank">
-            1250 Dupont St Toronto
+
+        <address className="text-center mt-5 not-italic">
+          <h3 className="text-lg sm:text-xl">
+            <a
+              className="hover:underline"
+              href="https://maps.app.goo.gl/rM8onc3vVnmvAodc9"
+              target="_blank"
+            >
+              1250 Dupont St Toronto
+            </a>
+          </h3>
+          <a
+            className="text-sm mt-2 hover:underline"
+            href="mailto:info@threehorses.cafe"
+          >
+            Contact Us
           </a>
-        </h3>
+        </address>
         <Instagram className="inline-block mt-10 drop-shadow-lg" />
       </main>
     </>
